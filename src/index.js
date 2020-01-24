8ee7bd84bcd70a2c46dafa864424982b0c9398d2
stayacid <stayacid@gmail.com>
2020-01-24 18:55:40 +0300

    doing

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './js/common'
+import App from '../src/App.vue'
+
+//import Vue
+import Vue from 'vue'
+
+new Vue({
+  render: h =>(App)
+}).$mount('#app')
+
+//style
+import './css/main.css'
+import './scss/main.scss'
+//import libs styles here
