a338be925f45a18a085e5fafbebbbac29f429370
stayacid <stayacid@gmail.com>
2020-01-27 21:52:00 +0300

    img compress work in build!

--- build/webpack.build.conf.js
+++ build/webpack.build.conf.js
@@ -1,9 +1,17 @@
 const merge = require('webpack-merge');
 const baseWebpackConfig = require('./webpack.base.conf');
+const ImageminPlugin = require('imagemin-webpack-plugin').default;
 
 const buildWebpackConfig = merge(baseWebpackConfig, {
   mode: 'production',
-  plugins: []
+  plugins: [
+    new ImageminPlugin({
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      pngquant: {
+        quality: '50'
+      }
+    })
+  ]
 })
 
 module.exports = new Promise((resolve, reject) => {
