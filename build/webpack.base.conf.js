2cc7d4a3957851e306b02f3c60628ae681a56917
stayacid <stayacid@gmail.com>
2020-01-27 18:44:10 +0300

    image optimization not working(

--- build/webpack.base.conf.js
+++ build/webpack.base.conf.js
@@ -3,12 +3,8 @@ const fs = require("fs");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const copyWebpackPlugin = require("copy-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const {
-  VueLoaderPlugin
-} = require("vue-loader");
-const {
-  CleanWebpackPlugin
-} = require('clean-webpack-plugin'); //clean dist folder before build
+const { VueLoaderPlugin } = require("vue-loader");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin'); //clean dist folder before build
 
 // Main const. Feel free to change it
 const PATHS = {
@@ -54,7 +50,17 @@ module.exports = {
       // PUG
       {
         test: /\.pug$/,
-        loader: 'pug-loader',
+        oneOf: [
+          // this applies to <template lang="pug"> in Vue components
+          {
+            resourceQuery: /^\?vue/,
+            use: ['pug-plain-loader']
+          },
+          // this applies to pug imports inside JavaScript
+          {
+            use: ['pug-loader']
+          }
+        ]
       },
       // JavaScript
       {
