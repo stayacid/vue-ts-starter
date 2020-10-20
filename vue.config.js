95b76c80cf70e154d01f54e0ae04220b539ec9e6
stayacid <stayacid@gmail.com>
2020-10-20 21:38:45 +0300

    still not works

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,29 @@
 module.exports = {
-  pages: {
+  /* configureWebpack: {
+    plugins: [
+      new HtmlWebpackPlugin({
+        template: '!!pug-loader!index.pug',
+        title: 'Page Title',
+        custom: 'Custom'
+      })
+    ]
+  }, */
+  chainWebpack: config => {
+    config
+      .plugin('html')
+      .tap(args => {
+        args[0].template = '!!pug-loader!index.pug'
+        args[0].title = 'Page Title'
+        args[0].template = 'public/index.pug'
+        return args
+      })
+  },
+  /* pages: {
     index: {
       entry: './src/main.ts',
       template: './public/index.pug'
     }
-  },
+  }, */
   css: {
     loaderOptions: {
       scss: {
